430f2d80e8e5c00685d97ae6b93be730101e3bb5
crtulloss <caleb.tulloss@columbia.edu>
2020-04-28 13:44:38 -0400

    modify backprop() and update_weights() to reflect autoencoder structure

--- backprop/backprop/backprop.c
+++ backprop/backprop/backprop.c
@@ -144,19 +144,15 @@ void get_delta_matrix_weights1(TYPE delta_weights1[input_dimension*layer1_dimens
 }
 
 void update_weights(TYPE weights1[input_dimension*layer1_dimension],
-                    TYPE weights2[layer1_dimension*layer1_dimension],
-                    TYPE weights3[layer1_dimension*output_dimension],
+                    TYPE weights2[layer1_dimension*output_dimension],
                     TYPE d_weights1[input_dimension*layer1_dimension],
-                    TYPE d_weights2[layer1_dimension*layer1_dimension],
-                    TYPE d_weights3[layer1_dimension*output_dimension],
+                    TYPE d_weights2[layer1_dimension*output_dimension],
                     TYPE biases1[layer1_dimension],
-                    TYPE biases2[layer1_dimension],
-                    TYPE biases3[output_dimension],
+                    TYPE biases2[output_dimension],
                     TYPE d_biases1[layer1_dimension],
-                    TYPE d_biases2[layer1_dimension],
-                    TYPE d_biases3[output_dimension]) {
+                    TYPE d_biases2[output_dimension]) {
     int i, j;
-    double norm, bias_norm;
+    TYPE norm, bias_norm;
     norm = 0.0;
     bias_norm = 0.0;
 
@@ -183,17 +179,17 @@ void update_weights(TYPE weights1[input_dimension*layer1_dimension],
         biases1[i] = (biases1[i]/bias_norm);
     }
 
-    norm = (double)0.0;
-    bias_norm = (double)0.0;
+    norm = (TYPE)0.0;
+    bias_norm = (TYPE)0.0;
 
     for(i=0; i < layer1_dimension; i++){
-        for(j = 0; j < layer1_dimension; j++){
-            weights2[i*layer1_dimension + j] -= (d_weights2[i*layer1_dimension + j] * learning_rate);
-            norm += weights2[i*layer1_dimension + j]*weights2[i*layer1_dimension + j];
+        for(j = 0; j < output_dimension; j++){
+            weights2[i*output_dimension + j] -= (d_weights2[i*output_dimension + j] * learning_rate);
+            norm += weights2[i*output_dimension + j]*weights2[i*output_dimension + j];
         }
     }
-    for(i=0; i < layer1_dimension; i++){
-        biases2[i] -= (d_biases2[i]*learning_rate);
+    for(i=0; i<output_dimension;i++){
+        biases2[i] -= d_biases2[i]*learning_rate;
         bias_norm += biases2[i]*biases2[i];
     }
 
@@ -201,38 +197,12 @@ void update_weights(TYPE weights1[input_dimension*layer1_dimension],
     bias_norm = sqrt(bias_norm);
 
     for(i=0; i < layer1_dimension; i++){
-        for(j = 0; j < layer1_dimension; j++){
-            weights2[i*layer1_dimension + j] = (weights2[i*layer1_dimension + j]/norm);
-        }
-    }
-    for(i=0; i < layer1_dimension; i++){
-        biases2[i] = (biases2[i]/bias_norm);
-    }
-
-    norm = (double)0.0;
-    bias_norm = (double)0.0;
-
-    for(i=0; i < layer1_dimension; i++){
         for(j = 0; j < output_dimension; j++){
-            weights3[i*output_dimension + j] -= (d_weights3[i*output_dimension + j] * learning_rate);
-            norm += weights3[i*output_dimension + j]*weights3[i*output_dimension + j];
-        }
-    }
-    for(i=0; i<output_dimension;i++){
-        biases3[i] -= d_biases3[i]*learning_rate;
-        bias_norm += biases3[i]*biases3[i];
-    }
-
-    norm = sqrt(norm);
-    bias_norm = sqrt(bias_norm);
-
-    for(i=0; i < layer1_dimension; i++){
-        for(j = 0; j < output_dimension; j++){
-            weights3[i*output_dimension + j] = (weights3[i*output_dimension + j]/norm);
+            weights2[i*output_dimension + j] = (weights2[i*output_dimension + j]/norm);
         }
     }
     for(i=0; i < output_dimension; i++){
-        biases3[i] = (biases3[i]/bias_norm);
+        biases2[i] = (biases2[i]/bias_norm);
     }
 }
 
@@ -242,44 +212,47 @@ void backprop(TYPE weights1[input_dimension*layer1_dimension],
                 TYPE biases2[output_dimension],
                 TYPE training_data[input_dimension]) {
     int i,j;
-    //Forward and training structures
+
+    // forward and training structures
     TYPE activations1[layer1_dimension];
-    TYPE activations2[layer1_dimension];
-    TYPE activations3[output_dimension];
+    TYPE activations2[output_dimension];
     TYPE dactivations1[layer1_dimension];
-    TYPE dactivations2[layer1_dimension];
-    TYPE dactivations3[output_dimension];
+    TYPE dactivations2[output_dimension];
     TYPE net_outputs[output_dimension];
-    //Training structure
+
+    // training structure
     TYPE output_difference[output_dimension];
     TYPE delta_weights1[input_dimension*layer1_dimension]; 
-    TYPE delta_weights2[layer1_dimension*layer1_dimension];
-    TYPE delta_weights3[layer1_dimension*output_dimension];
+    TYPE delta_weights2[layer1_dimension*output_dimension];
     TYPE oracle_activations1[layer1_dimension];
-    TYPE oracle_activations2[layer1_dimension];
 
-    for(i=0; i<training_sets; i++){
-        for(j=0;j<layer1_dimension;j++){
+    // same training data is used for num_iters_perin
+    for (i = 0; i < num_iters_perin; i++) {
+
+        // reset activations at the beginning of each iteration
+        for (j = 0; layer1_dimension; j++) {
             activations1[j] = (TYPE)0.0;
-            activations2[j] = (TYPE)0.0;
-            if(j<output_dimension){
-                activations3[j] = (TYPE)0.0;
-            }
         }
-        matrix_vector_product_with_bias_input_layer(biases1, weights1, activations1, &training_data[i*input_dimension]);
+        for (j = 0; j < output_dimension; j++) {
+            activations2[j] = (TYPE)0.0;
+        }
+
+        matrix_vector_product_with_bias_input_layer(biases1, weights1, activations1, &training_data[input_dimension]);
         RELU(activations1, dactivations1, layer1_dimension);
-        matrix_vector_product_with_bias_second_layer(biases2, weights2, activations2, activations1);
-        RELU(activations2, dactivations2, layer1_dimension);
-        matrix_vector_product_with_bias_output_layer(biases3, weights3, activations3, activations2);
-        RELU(activations3, dactivations3, output_dimension);
-        soft_max(net_outputs, activations3);
-        take_difference(net_outputs, &training_targets[i*output_dimension], output_difference, dactivations3);
-        get_delta_matrix_weights3(delta_weights3, output_difference, activations2);
-        get_oracle_activations2(weights3, output_difference, oracle_activations2, dactivations2);
-        get_delta_matrix_weights2(delta_weights2, oracle_activations2, activations1);
-        get_oracle_activations1(weights2, oracle_activations2, oracle_activations1, dactivations1);
-        get_delta_matrix_weights1(delta_weights1, oracle_activations1, &training_data[i*input_dimension]);
-        update_weights(weights1, weights2, weights3, delta_weights1, delta_weights2, delta_weights3, 
-                       biases1, biases2, biases3, oracle_activations1, oracle_activations2, output_difference);
+
+        matrix_vector_product_with_bias_output_layer(biases2, weights2, activations2, activations1);
+        RELU(activations2, dactivations2, output_dimension);
+
+        soft_max(net_outputs, activations2);
+        
+        take_difference(net_outputs, &training_data[output_dimension], output_difference, dactivations2);
+
+        get_delta_matrix_weights3(delta_weights2, output_difference, activations1);
+        get_oracle_activations2(weights2, output_difference, oracle_activations1, dactivations1);
+        
+        get_delta_matrix_weights1(delta_weights1, oracle_activations1, &training_data[input_dimension]);
+
+        update_weights(weights1, weights2, delta_weights1, delta_weights2, 
+                       biases1, biases2, oracle_activations1, output_difference);
     }
 }
